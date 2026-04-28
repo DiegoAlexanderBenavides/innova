560ffc9b4667e739fc0fd8a2a545013d159a77b7
Diego Benavides <diegoven@unicauca.edu.co>
2026-04-28 10:27:24 -0500

    Primer commit (migracion de la pagina web a astron)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/DiegoAlexanderBenavides',
+  base: '/innova',
+
+  output: 'static',
+
   vite: {
     plugins: [tailwindcss()]
   }
